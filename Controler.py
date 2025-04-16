fcd788fe5b7437ae11e36d900c4ebca8140f3ce4
Luís Barata <luisbaptistab@gmail.com>
2025-04-16 10:51:25 +0100

    Controler.py

--- Controler.py
+++ Controler.py
@@ -1,1 +1,44 @@
-import 
+import paho.mqtt.client as mqtt
+
+
+BROKER = "broker.hivemq.com"
+PORT = 1883
+TOPIC = "machine/controller"
+
+
+def on_connect(client, userdata, flags, rc):
+    print(f"[CONNECTED] Return code: {rc}")
+    client.subscribe(TOPIC)
+    print(f"[SUBSCRIBED] to topic '{TOPIC}'")
+
+
+def on_message(client, userdata, msg):
+    print(f"[MESSAGE] {msg.topic}: {msg.payload.decode()}")
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(BROKER, PORT)
+client.loop_start()
+
+welcome = """\
+======== CONTROL CONSOLE ===========
+========= TYPE 0 to turn OFF
+========= TYPE 1 to turn ON ===========
+"""
+print(welcome)
+
+import time
+
+try:
+    while True:
+        comand = input()
+        if comand not in ("0", "1"):
+            print("Command not allowed, try 1 or 0\n")
+        else:
+            client.publish(TOPIC, comand)
+            print("[Message] sent")
+except KeyboardInterrupt:
+    print(" CLOSING...")
