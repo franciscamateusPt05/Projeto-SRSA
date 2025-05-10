013cfabacd69d962a35c8875a4c19ec98a828349
Luís Barata <luisbaptistab@gmail.com>
2025-05-10 13:14:48 +0100

    Machine Simulator inicio

---
+++ Parte2/MachineSimulator.py
@@ -0,0 +1,119 @@
+import sys
+import time
+import json
+import random 
+from datetime import datetime
+import paho.mqtt.client as mqtt
+
+# Config do ficheiro
+GroupID = sys.argv[1]
+MACHINE_UPDATE_TIME = sys.argv[2] # pode ser alterado 
+Machine_Code = sys.argv[3]
+
+# TOPICOS
+# para enviar
+network_topic = f'v3/{GroupID}@ttn/devices/{MachineID}/up'
+# para receber
+DataManagerAgent_topic = f'v3/{GroupID}@ttn/devices/{MahcineID}/down/push_actuator'
+AlertManger_topic = f'v3/{GroupID}@ttn/devices/{MachineID}/down/push_alert'
+
+
+mqtthost = "10.6.1.9"
+mqttport = 1883
+
+
+client = mqtt.Client()
+
+
+
+unidades = {"OilPressure":[0,1,0,1,0,1,0,1],
+            "CoolantTemp":[1,1,1,1,0,0,0,0],
+            "BatteryPotential":[1,1,1,1,1,1,1,1],
+            "Consumption":[1,0,0,1,0,1,1,0]
+            }
+
+CodeMachine={"A23X":1,"B47Y":2,"C89Z":3,"D56W":4,"E34V":5,"F78T":6,"G92Q":7,"H65P":8}
+
+Machine_Data= { 
+  "end_device_ids": { 
+    "machine_id": f'M{CodeMachine[Machine_Code]}', 
+    "application_id": "my-application", 
+    "dev_eui": "70B3D57ED00347C5", 
+    "join_eui": "0000000000000000", 
+    "dev_addr": "260B1234" 
+  }, 
+  "received_at": datetime.now(), 
+  "uplink_message": { 
+    "f_port": 1, 
+    "f_cnt": 1234, 
+    "frm_payload": "BASE64_ENCODED_PAYLOAD", 
+    "decoded_payload": { 
+      "rpm": 2000.0, 
+      "coolant_temperature": 92.0, 
+      "oil_pressure": 3.2, 
+      "battery_potential": 12.6, 
+      "consumption": 15.8, 
+      "machine_type": Machine_Code 
+    }, 
+    "rx_metadata": [ 
+      { 
+        "gateway_id": "gateway-1", 
+        "rssi": -75, 
+        "snr": 9.2, 
+        "channel_rssi": -76, 
+        "uplink_token": "TOKEN_VALUE" 
+      } 
+    ], 
+    "settings": { 
+      "data_rate": { 
+        "modulation": "LORA", 
+        "bandwidth": 125000, 
+        "spreading_factor": 7 
+      }, 
+      "frequency": "868300000", 
+      "timestamp": 1234567890 
+    }, 
+    "consumed_airtime": "0.061696s" 
+  } 
+}
+
+def normalizeTemp(Ftemperature):
+    pass
+
+def normalizeOil(PsiOil):
+    pass
+
+def normalizePotential(mV):
+    pass
+
+def normalizeConsumption(gal):
+    pass 
+
+
+def generatenewdata():
+  global Machine_Data
+  rpm =               Machine_Data["decoded_payload"]["rpm"] + random.choice([-50,200])
+  oilpressure =       Machine_Data["decoded_payload"]["oil_pressure"] +random.choice([-0.1,0.5])
+  batterypontential = Machine_Data["decoded_payload"]["battery_pontential"] + random.choice([-0.1,0.2])
+  consumption =       Machine_Data["decoded_payload"]["consumption"] + random.choice([-1,1])
+  coolanttemp =       Machine_Data["decoded_payload"]["coolant_temperature"] + random.choice([-0.3,1.0])
+    
+  Machine_Data["decoded_payload"]["rpm"] = max(800,min(rpm,3000))
+  Machine_Data["decoded_payload"]["oil_pressure"] = max(1.5,min(oilpressure,8.0))
+  Machine_Data["decoded_payload"]["battery_pontential"] = max(10,min(batterypontential,14))
+  Machine_Data["decoded_payload"]["consumption"] = max(1,min(consumption,50))
+  Machine_Data["decoded_payload"]["coolant_temperature"] = max(70,min(coolanttemp,130))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
