9b7b0534f7cf9bf9a5a3633c27eb2042c0571f15
Stew <luis@gmail.com>
2025-05-15 07:37:14 +0100

    Debugger

---
+++ Parte2/Mqttdebuger.py
@@ -0,0 +1,18 @@
+import paho.mqtt.client as mqtt
+import datetime 
+topic = 'v3/{GroupID}@ttn/devices/#'
+broker = '10.6.1.9'
+port = 1883
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+    client.subscribe(topic)
+
+def on_message(client, userdata, message):
+    print(f" {datetime.now()} {message.topic}  {message.payload.decode()}")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(broker, port)
+client.loop_forever()
